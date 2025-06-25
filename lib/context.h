9442e747b044067385347e8760f960003c0473bb
keringo <>
2025-06-25 20:50:17 +0300

    added black_hex and white_hex fields in context

--- lib/context.h
+++ lib/context.h
@@ -50,6 +50,8 @@ typedef struct KOPTContext {
     int read_max_width;
     int read_max_height;
     int writing_direction;
+	int black_hex;
+	int white_hex;
 
     double zoom;
     double margin;
